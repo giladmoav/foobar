d9cc3c4b600a4196e4d6bf1f62414772eb8e127d
Gilad <Gilad@foobar.com>
2025-06-30 20:32:28 +0300

    Improve my strace

--- my_strace.c
+++ my_strace.c
@@ -1,3 +1,4 @@
+#include "my_strace.h"
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -6,8 +7,26 @@
 #include <sys/wait.h>
 #include <sys/user.h>
 
-#define ERROR_EXIT_STATUS -1
 
+/// print information on syscall based on syscall number, syscall args and the return value
+void print_syscall(unsigned long long syscall_number, unsigned long long syscall_args[], unsigned long long return_value) {
+    unsigned char arg_count;
+    unsigned char i;
+    if (syscall_number < SYSCALL_LIST_LENGTH) {
+        printf("%s(", syscalls[syscall_number]);
+        arg_count = syscalls_arg_count[syscall_number];
+    } else {
+        printf("unknown-syscall-%llu(", syscall_number);
+        arg_count = MAX_ARG_COUNT;
+    }
+    if (arg_count > 0) {
+        for (i = 0; i < arg_count - 1; i++) {
+            printf("%llu, ", syscall_args[i]);
+        }
+        printf("%llu", syscall_args[i]);
+    }
+    printf(") = %llu\n", return_value);
+}
 
 /// trace the process described by pid and print syscalls made by it
 void handle_tracing(pid_t pid) {
@@ -15,6 +34,7 @@ void handle_tracing(pid_t pid) {
     unsigned long long syscall_number;
     unsigned long long return_value;
     struct user_regs_struct regs;
+    unsigned long long syscall_args[6];
 
     // wait for child process start
     waitpid(pid, &status, 0);
@@ -32,6 +52,12 @@ void handle_tracing(pid_t pid) {
 
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
         syscall_number = regs.orig_rax;
+        syscall_args[0] = regs.rdi;
+        syscall_args[1] = regs.rsi;
+        syscall_args[2] = regs.rdx;
+        syscall_args[3] = regs.r10;
+        syscall_args[4] = regs.r8;
+        syscall_args[5] = regs.r9;
 
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         waitpid(pid, &status, 0);
@@ -42,7 +68,7 @@ void handle_tracing(pid_t pid) {
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
         return_value = regs.rax;
         
-        printf("%llu -> %llu\n", syscall_number, return_value);
+        print_syscall(syscall_number, syscall_args, return_value);
     }
 }
 
@@ -61,14 +87,5 @@ void strace(char* argv[], char* argp[]) {
 
     // parent process
     handle_tracing(pid);
-}
-
-void main(int argc, char* argv[], char* argp[]) {
-    if (argc < 2) {
-        printf("Usage: %s BINARY ARGS\n", argv[0]);
-        exit(ERROR_EXIT_STATUS);
-    }
-
-    strace(argv + 1, argp);
 }
 
